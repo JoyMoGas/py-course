b3407cd309d188d015eca659eabe4c2bd3f07b8a
JoyMoGas <joseantoniomontanogastelum5@gmail.com>
2024-11-28 23:10:11 -0700

    section16 finished

---
+++ section16/myProgram.py
@@ -0,0 +1,39 @@
+"""
+* Class 144 'SYNTAX ERRORS'
+print(1)
+int(9)
+int(000)
+print(2)
+print(3)
+a = [1,2,3}]
+"""
+
+"""
+* Class 145 'EXCEPTIONS'
+a = 1
+b = '2'
+print(int(2.5)
+print(a + b)
+"""
+
+"""
+* Class 146 'HOW TO FIX DIFFICULT ERRORS'
+How to search in internet for the error
+a = 1
+b = "2"
+print(int(2.5))
+
+"""
+
+"""
+* Class 148
+"""
+
+def divide(a,b):
+  try:
+    return a/b
+  except ZeroDivisionError:
+    return('Zero division is meaningless')
+
+print(divide(1,2))
+print('End of program')
